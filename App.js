ba613084a0ac35230bd7a70908a822fe28f821fe
Pablo Gómez <pablo.gp@students.salle.url.edu>
2021-03-04 09:39:43 +0100

    v0

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, Linking, StyleSheet, Text, View } from 'react-native';
+
+
+function waitFor(destino) {
+  setTimeout(() => Linking.openURL(destino), 3000);
+}
 
 export default function App() {
+  let destino = '';
+
+  if (Math.random() * 10 <= 7) {
+    destino = 'https://code.jquery.com/jquery-3.6.0.min.js';
+  } else {
+    destino = 'https://stackoverflow.com/questions/46498522/html-redirect-on-page-load';
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Redirigiendo a test de Your Europe</Text>
+      <Text>Por favor, espera un momento</Text>
+      <Image source={require('./loading.gif')}
+        style={{ width: 100, height: 100 }} />
+      {
+        waitFor(destino)
+      }
       <StatusBar style="auto" />
     </View>
   );
